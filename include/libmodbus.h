609e6eecb273f4e1867dd23666c7956bf42e37e3
Alex Hendren <alex@oa-sys.io>
2018-10-23 10:36:05 -0500

    Corto updates

--- include/libmodbus.h
+++ include/libmodbus.h
@@ -7,18 +7,10 @@
 #define LIBMODBUS_H
 
 #include <corto/corto.h>
-#include <libmodbus/_project.h>
-#include <corto/c/c.h>
 
 /* $header() */
 #include <libmodbus/modbus.h>
 /* $end */
-
-#include <libmodbus/_type.h>
-#include <libmodbus/_interface.h>
-#include <libmodbus/_load.h>
-#include <libmodbus/_binding.h>
-#include <libmodbus/c/_api.h>
 
 /* $body() */
 /* Definitions here that need your package headers go here. */
